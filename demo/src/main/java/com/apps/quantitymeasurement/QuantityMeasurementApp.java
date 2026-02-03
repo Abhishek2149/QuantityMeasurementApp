e18458995f652a0874e2ac665bff5d68e203054c
Abhishek2149 <abhi.jaybhaye2000@gmail.com>
2026-02-03 17:22:45 +0530

    Generic Quantity

--- demo/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ demo/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -2,6 +2,8 @@ package com.apps.quantitymeasurement;
 
 import java.util.Scanner;
 
+import com.apps.quantitymeasurement.Length.LengthUnit;
+
 /**
  * Hello world!
  *
@@ -63,76 +65,79 @@ public class QuantityMeasurementApp {
         }
     }
 
+    // Length equality demonstration
+    public static boolean demonstrateLengthEquality(Length length1, Length length2) {
+        return length1.equals(length2);
+    }
+
+
+    //Helper method to read Length from user input
+
+    private static Length readLength(Scanner scanner) {
+        System.out.print("Enter length value: ");
+        double value = scanner.nextDouble();
+
+        System.out.print("Enter length unit (INCHES, FEET): ");
+        String unitInput = scanner.next().toUpperCase();
+        LengthUnit unit;
+        try {
+            unit = LengthUnit.valueOf(unitInput);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid length unit");
+        }
+
+        return new Length(value, unit);
+
+    }
+
+    //Feet equality demonstration
     public static void demonstrateFeetEquality(Scanner scanner) {
-        double value1 = 0;
-        double value2 = 0;
+        Length length1 = readLength(scanner);
+        Length length2 = readLength(scanner);
 
-        // Taking first input
-        System.out.print("Enter first value in feet: ");
-        if (scanner.hasNextDouble()) {
-            value1 = scanner.nextDouble();
-        } else {
-            System.out.println("Invalid input! Please enter a numeric value.");
-            return;
-        }
+        System.out.println("Input: Quantity 1: " + length1.getValue() + " " + length1.getUnit().name().toLowerCase() +
+                ", Quantity 2: " + length2.getValue() + " " + length2.getUnit().name().toLowerCase());
+       
+        
 
-        // Taking second input
-        System.out.print("Enter second value in feet: ");
-        if (scanner.hasNextDouble()) {
-            value2 = scanner.nextDouble();
-        } else {
-            System.out.println("Invalid input! Please enter a numeric value.");
-            return;
-        }
-
-        Feet feet1 = new Feet(value1);
-        Feet feet2 = new Feet(value2);
-
-        boolean result = feet1.equals(feet2);
-
-        System.out.println("\nResult: Equal (" + result + ")");
+        System.out.println("\nOutput: Equal (" + demonstrateLengthEquality(length1, length2) + ")");
     }
 
     public static void demonstrateInchEquality(Scanner scanner) {
-        double value1 = 0;
-        double value2 = 0;
+        Length length1 = readLength(scanner);
+        Length length2 = readLength(scanner);
 
-        // Taking first input
-        System.out.print("Enter first value in inches: ");
-        if (scanner.hasNextDouble()) {
-            value1 = scanner.nextDouble();
-        } else {
-            System.out.println("Invalid input! Please enter a numeric value.");
-            return;
-        }
+        System.out.println("Input: Quantity 1: " + length1.getValue() + " " + length1.getUnit().name().toLowerCase() +
+                ", Quantity 2: " + length2.getValue() + " " + length2.getUnit().name().toLowerCase());
+       
+        
 
-        // Taking second input
-        System.out.print("Enter second value in inches: ");
-        if (scanner.hasNextDouble()) {
-            value2 = scanner.nextDouble();
-        } else {
-            System.out.println("Invalid input! Please enter a numeric value.");
-            return;
-        }
+        System.out.println("\nOutput: Equal (" + demonstrateLengthEquality(length1, length2) + ")");
 
-        Inches inches1 = new Inches(value1);
-        Inches inches2 = new Inches(value2);
+        
+    }
 
-        boolean result = inches1.equals(inches2);
+    public static void demonstrateFeetinchesComparison(Scanner scanner) {
+        Length lengthInFeet = readLength(scanner);
+        Length lengthInInches = readLength(scanner);
 
-        System.out.println("\nResult: Equal (" + result + ")");
+        System.out.println("Input: Quantity 1: " + lengthInFeet.getValue() + " " + lengthInFeet.getUnit().name().toLowerCase() +
+                ", Quantity 2: " + lengthInInches.getValue() + " " + lengthInInches.getUnit().name().toLowerCase());
+
+        System.out.println("\nOutput: Equal (" + demonstrateLengthEquality(lengthInFeet, lengthInInches) + ")");
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         try (scanner) {
-            System.out.println("Demonstrating Feet Equality:");
+            System.out.println("Demonstrating Feet Equality (Enter values in feet):");
             demonstrateFeetEquality(scanner);
 
-            System.out.println("\nDemonstrating Inches Equality:");
+            System.out.println("\nDemonstrating Inches Equality (Enter values in inches):");
             demonstrateInchEquality(scanner);
-        }
-        finally {
+            System.out.println("\nDemonstrating Feet and Inches Comparison:");
+            demonstrateFeetinchesComparison(scanner);
+        } finally {
             scanner.close();
         }
     }
